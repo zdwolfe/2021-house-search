1bdc83fa5c60772ca4495f2cc7671235c84e217c
Zach Wolfe <wolfe.zach@gmail.com>
2021-05-20 11:20:10 -0700

    Add values to node labels

--- graphs.py
+++ graphs.py
@@ -31,16 +31,16 @@ class Graph:
         # ["zillow: 4"]
         domain_labels = [Graph.to_domain_label(domain_label_datum) for domain_label_datum in domain_label_data]
 
-        destination_labels = ["Viewed Listing", "Considered", "Requested Tour", "Scheduled Tour",
-                              "Toured", "Requested Application",
-                              "Applied", "Abandoned", "Accepted"]
+        destination_labels = ["Viewed Listing: 432", "Considered: 57", "Requested Tour: 22", "Scheduled Tour: 6",
+                              "Toured: 3", "Requested Application",
+                              "Applied: 8", "Abandoned: 46", "Accepted: 1"]
         labels = domain_labels + destination_labels
 
 
         listing_viewed_edges = [
             (
                 labels.index(Graph.to_domain_label(domain_datum)),
-                labels.index("Viewed Listing"),
+                labels.index("Viewed Listing: 432"),
                 len(browser_history[domain_datum[0]])
             )
             for domain_datum in domain_label_data
@@ -51,68 +51,68 @@ class Graph:
         listing_viewed_values = source_target_value_listing_edges[2]
 
         listing_viewed_to_considered = (
-            labels.index("Viewed Listing"),
-            labels.index("Considered"),
+            labels.index("Viewed Listing: 432"),
+            labels.index("Considered: 57"),
             22 + 31  # applied + "not interested" cards in trello
         )  # from trello and gmail
 
         considered_to_treq = (
-            labels.index("Considered"),
-            labels.index("Requested Tour"),
+            labels.index("Considered: 57"),
+            labels.index("Requested Tour: 22"),
             22
         )  # from trello and gmail
 
         considered_to_not_interested = (
-            labels.index("Considered"),
-            labels.index("Abandoned"),
+            labels.index("Considered: 57"),
+            labels.index("Abandoned: 46"),
             31
         )  # from trello and gmail
 
         requested_tour_to_scheduled = (
-            labels.index("Requested Tour"),
-            labels.index("Scheduled Tour"),
+            labels.index("Requested Tour: 22"),
+            labels.index("Scheduled Tour: 6"),
             6
         )  # from gcal
 
         requested_tour_to_abandoned = (
-            labels.index("Requested Tour"),
-            labels.index("Abandoned"),
+            labels.index("Requested Tour: 22"),
+            labels.index("Abandoned: 46"),
             5
         )  # from gcal
 
         scheduled_to_toured = (
-            labels.index("Scheduled Tour"),
-            labels.index("Toured"),
+            labels.index("Scheduled Tour: 6"),
+            labels.index("Toured: 3"),
             3
         )  # from memory
 
         scheduled_to_abandoned = (
-            labels.index("Scheduled Tour"),
-            labels.index("Abandoned"),
+            labels.index("Scheduled Tour: 6"),
+            labels.index("Abandoned: 46"),
             3
         )  # from gcal
 
         considered_applied = (
-            labels.index("Considered"),
-            labels.index("Applied"),
+            labels.index("Considered: 57"),
+            labels.index("Applied: 8"),
             4
         )  # from gmail
 
         toured_to_applied = (
-            labels.index("Toured"),
-            labels.index("Applied"),
+            labels.index("Toured: 3"),
+            labels.index("Applied: 8"),
             3
         )  # from memory
 
         applied_to_abandoned = (
-            labels.index("Applied"),
-            labels.index("Abandoned"),
+            labels.index("Applied: 8"),
+            labels.index("Abandoned: 46"),
             7
         )  # from memory
 
         applied_to_accepted = (
-            labels.index("Applied"),
-            labels.index("Accepted"),
+            labels.index("Applied: 8"),
+            labels.index("Accepted: 1"),
             1
         )  # from memory
 
